94b4a725a50379c61a2bff33da700435caa11772
Sollykruzz <144743239+Sollykruzz@users.noreply.github.com>
2025-09-02 15:23:14 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+function swap() {
+    alert('Swap feature coming soon with Enso API integration!');
+}
+
+function bridge() {
+    alert('Bridge feature coming soon with Enso API integration!');
+}
